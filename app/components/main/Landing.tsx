d856cacb820d91c725fbb06d3fb8c6abc88be04b
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-02 13:05:04 -0800

    refactor: extract JSX to RFC

--- app/components/main/Landing.tsx
+++ app/components/main/Landing.tsx
@@ -2,34 +2,12 @@
 
 import React from 'react';
 
-import { DictionaryEntryParser } from '@/app/utils/core/DictionaryEntryParser';
-import { Payload } from '@/app/utils/core/types/DictionaryEntryParser.types';
+import SearchBar from '@/forward/SearchBar';
 
 export default function Landing() {
-  const [wordData, setWordData] = React.useState<unknown>(null);
-  const [parsedWordData, setParsedWordData] = React.useState<unknown>([]);
-
-  React.useEffect(() => {
-    if (wordData !== null) {
-      const parsed = DictionaryEntryParser.parse(wordData);
-      const items = [];
-      parsed.forEach(seq => seq.defs.forEach(def => items.push(def[1])));
-      setParsedWordData(items);
-    }
-  }, [wordData]);
-
   return (
     <>
-      <button 
-        type="button"
-        onClick={() => setWordData(() => fetch('http://localhost:3000/api/v1', {
-          method: 'POST',
-          body: JSON.stringify('light')
-        }).then(res => res.json().then(res => setWordData(res))))}
-      >
-        Hi
-      </button>
-      {parsedWordData}
+      <SearchBar />
     </>
   )
 }

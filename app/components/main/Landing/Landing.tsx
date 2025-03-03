813c5d7bbcfbb13e215092e7c57d1d964a3ba2df
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-02 21:01:18 -0800

    path: modularize Landing RFC

---
+++ app/components/main/Landing/Landing.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+import SearchBar from '@/forward/navigation/SearchBar';
+
+export default function Landing() {
+  return (
+    <>
+      <SearchBar />
+    </>
+  )
+}
+
---
+++ app/components/main/Landing/index.ts
@@ -0,0 +1,7 @@
+import Landing from './Landing';
+
+export default Landing;
+
+export {
+
+}

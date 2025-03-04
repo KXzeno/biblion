eb09dcfa26ab3b0ca1e0b9c7de30c6d484fae666
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-04 06:25:46 -0800

    docs: mark deprecated

--- app/utils/core/Reactor/Reactor.tsx
+++ app/utils/core/Reactor/Reactor.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+// import StringLiteralBuilder from "@/utils/auxil/StringLiteralBuilder";
 
 /**
  * The Reactor class creates strongly typed 
  * byproducts within React's ecosystem with the 
  * core intention to reduce unnecessary boilerplate 
  *
+ * @remarks
+ *
+ * After days of effort playing with conditional and 
+ * template literal types, I've yet to find a solution
+ * to convert parsed strings to literals. As of now this
+ * class will be deprecated until picked up for relevant use
+ *
+ * @deprecated
+ *
  * @author Kx
  */
 export default class Reactor {
-  private fn;
+  // private fn;
 
   /**
    * Instantiates with the reducer callback
@@ -24,50 +33,37 @@ export default class Reactor {
    *
    * @returns **TODO**
    */
-  private parseActionTypes(): string[] {
-    const fnString = this.fn.toString();
+  // private parseActionTypes(): string[] {
+  //   const fnString = this.fn.toString();
 
-    const params = fnString.match(/(?!\([\s]?)([\w\s\,]+)(?=[\s]?\))/);
+  //   const params = fnString.match(/(?!\([\s]?)([\w\s\,]+)(?=[\s]?\))/);
 
-    if (params && params[0].split(/\,/).length !== 2) {
-      throw new Error('Arguments unfulfilled.');
-    }
+  //   if (params && params[0].split(/\,/).length !== 2) {
+  //     throw new Error('Arguments unfulfilled.');
+  //   }
 
-    let types: RegExpStringIterator<RegExpMatchArray> | string[] = fnString.matchAll(/(?!case\:\s\')([\w]+)(?=\':)/g);
+  //   let types: RegExpStringIterator<RegExpMatchArray> | string[] = fnString.matchAll(/(?!case\:\s\')([\w]+)(?=\':)/g);
 
-    try {
-      types = types.map(type => type[0]).toArray();
-      return types;
-    } catch (err) {
-      throw new Error(`Failed to parse types: ${err}`);
-    }
-  }
+  //   try {
+  //     types = types.map(type => type[0]).toArray();
+  //     return types;
+  //   } catch (err) {
+  //     throw new Error(`Failed to parse types: ${err}`);
+  //   }
+  // }
 
   /**
    * Customized reducer that automates static typing
    *
    * @param fn - the reducer callback
    */
-  public static useReducer(fn: (state: object, action: { type: string }) => object) {
-    const reactor = new Reactor(fn);
-    let arr = ['what'];
-    const actionTypeArray: Array<string> = reactor.parseActionTypes();
-    // console.log(actionTypeArray);
-
-    // FIXME: Current problem is ActionTypes results to as never due to 
-    // RefArray holding strings, not string literals.
-    // 
-    // Create an enum or a positional list and parse the string for all the
-    // enum properties or list positions then do a literal construction via
-    // template string
-    type ActionTypes<T, RefArray extends string[], Indexer extends number[] = []> = Indexer['length'] extends RefArray['length'] ? 
-      Exclude<T, null> :
-      ActionTypes<T | RefArray[Indexer['length']], RefArray, [...Indexer, Indexer['length']]>;
-
-    // const types: ActionTypes<null, typeof actionTypeArray>;
-
-    // return [types, ''];
-  }
-
-  // Functions must have @internal comments, like /** @internal {opt} */
+  // public static useReducer(fn: (state: object, action: { type: string }) => object) {
+  //    const reactor = new Reactor(fn);
+  //    const actionTypeArray: Array<string> = reactor.parseActionTypes();
+  // }
 }
+
+// type ActionTypes<T, RefArray extends string[], Indexer extends number[] = []> = Indexer['length'] extends RefArray['length'] ? 
+//   Exclude<T, null> :
+//   ActionTypes<T | RefArray[Indexer['length']], RefArray, [...Indexer, Indexer['length']]>;
+

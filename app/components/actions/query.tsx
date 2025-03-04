5725d25cbd56a99a8dc9b3570cbb2a58456ff0d6
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-04 06:01:53 -0800

    feat: implement form data validation

---
+++ app/components/actions/query.tsx
@@ -0,0 +1,24 @@
+'use server';
+
+import { z } from "zod";
+import { redirect } from "next/navigation";
+
+export async function queryWord(prevState: { msg: string }, formData: FormData) {
+  const word = formData.get('word');
+
+  let success = true;
+
+  const wordData = await fetch('http://localhost:3000/api/v1', {
+    method: 'POST',
+    body: JSON.stringify(word),
+  }).then(res => res.json()).catch(() => success = false);
+
+  if (!success || !word || wordData.length === 0 || typeof wordData[0] === 'string') {
+    return { 
+      msg: `${word.toString().toLowerCase()}`,
+      similar: wordData.filter(word => typeof word === 'string'),
+    };
+  }
+
+  redirect(`/dictionary/${word.toString().toLowerCase()}`);
+}

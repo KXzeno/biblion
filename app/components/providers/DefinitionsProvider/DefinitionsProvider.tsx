14fb307543d622078b4e2c008d38922ce02ffbf2
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-04 15:09:28 -0800

    REFACTOR: modernize defaults for new types

--- app/components/providers/DefinitionsProvider/DefinitionsProvider.tsx
+++ app/components/providers/DefinitionsProvider/DefinitionsProvider.tsx
@@ -8,14 +8,21 @@ import {
 import { queryWord } from '@/actions/query';
 // import DictionaryEntryParser, { type Payload } from '@/utils/core/DictionaryEnteryParser';
 import type { SearchBarReducState, SearchBarReducAction } from '@/components/forward/navigation';
+import { ActionType, PayloadStatus } from '@/forward/navigation/SearchBar/SearchBar.types';
 
 export const DefinitionsContext: React.Context<ContextData> = React.createContext({
   formState: { 
-    msg: '', 
-    similar: [''],
+    msg: '' as string, 
+    similar: [] as string[],
+    rawData: [] as object[],
   },
   formAction: (() => null) as (payload: FormData) => void,
-  reducState: { input: '' },
+  reducState: { 
+    input: '' as string,
+    CLIENT_CACHE: [] as string[],
+    rawData: [] as object[],
+    status: PayloadStatus.INACTIVE as PayloadStatus
+  },
 });
 
 export const DefinitionsDispatchContext: React.Context<DispatchContextData> = React.createContext({
@@ -25,31 +32,52 @@ export const DefinitionsDispatchContext: React.Context<DispatchContextData> = Re
 
 function reducer(state: SearchBarReducState, action: SearchBarReducAction) {
   switch (action.type) {
-    case 'query': {
+    case ActionType.Query: {
       if (!action.payload) {
         throw new Error('Payload empty.');
       }
 
       // const input = action.payload.input
 
-      return { ...state, input: '' };
+      return { 
+        input: '',
+        CLIENT_CACHE: [...state.CLIENT_CACHE as string[], state.input], 
+        rawData: action.payload.rawData,
+        status: PayloadStatus.SUCESS,
+      };
     }
-    case 'input': {
+    case ActionType.Input: {
       if (!action.payload) {
         return { ...state };
       }
 
       return { ...state, input: action.payload.input };
     }
-    case 'invalidate': {
+    case ActionType.Invalidate: {
+      if (state.status === PayloadStatus.INACTIVE) {
+        return { ...state };
+      }
       return { ...state };
     }
+    case ActionType.Inject: {
+      if (!action.payload) {
+        throw new Error('No payload to parse.');
+      }
+      return { ...state, rawData: action.payload.rawData };
+    }
+    default: return { ...state };
   }
 }
 
 export default function DefinitionsProvider({ children }: { children: React.ReactNode }): React.ReactNode {
-  const [formState, formAction, ] = React.useActionState(queryWord, { msg: '', similar: [] });
-  const [reducState, dispatch] = React.useReducer<SearchBarReducState, [SearchBarReducAction]>(reducer, { input: '' });
+  const [formState, formAction, ] = React.useActionState(queryWord, { msg: '', similar: [] as string[], rawData: [] as object[] });
+
+  const [reducState, dispatch] = React.useReducer<SearchBarReducState, [SearchBarReducAction]>(reducer, { 
+    input: '' as string, 
+    CLIENT_CACHE: [] as string[], 
+    rawData: [] as object[],
+    status: PayloadStatus.INACTIVE as PayloadStatus,
+  });
 
   return (
     <DefinitionsContext.Provider value={{ formState, formAction, reducState }}>

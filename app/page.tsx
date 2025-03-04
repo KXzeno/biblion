376c4a14c40850dcc19558dfd4574d7cf28e9186
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-04 06:25:30 -0800

    chore: remove unused dep

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import { DictionaryEntryParser } from "./utils/core/DictionaryEntryParser";
-
-import 'dotenv/config';
 import Landing from "./components/main/Landing";
 
 export default async function Home() {
-  // const test = await fetch('http://localhost:3000/api/v1');
-  // console.log(await test.blob().then(res => res.text()));
 
   return (
     <>

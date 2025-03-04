468334f270216bb8292f83e15c040955025d48b1
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-04 15:03:31 -0800

    refactor: remove server redirect
    - Forces client refresh which removes context states
    + Enforce router from navigation

--- app/components/actions/query.tsx
+++ app/components/actions/query.tsx
@@ -1,9 +1,9 @@
 'use server';
 
 // import { z } from "zod";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
-export async function queryWord(prevState: { msg: string }, formData: FormData) {
+export async function queryWord(prevState: { msg: string, similar: string[], rawData: object[] }, formData: FormData) {
   const word = formData.get('word');
 
   let success = true;
@@ -14,13 +14,19 @@ export async function queryWord(prevState: { msg: string }, formData: FormData) 
   }).then(res => res.json()).catch(() => success = false);
 
   if (!success || !word || wordData.length === 0 || typeof wordData[0] === 'string') {
-    if (!word) { return { msg: '', similar: [] }; }
+    if (!word) { console.log(3); return { msg: '', similar: [], rawData: [{}] }; }
 
     return { 
       msg: `${word.toString().toLowerCase()}`,
       similar: wordData.filter((word: string) => typeof word === 'string'),
+      rawData: wordData,
     };
   }
 
-  redirect(`/dictionary/${word.toString().toLowerCase()}`);
+  return { 
+    msg: ``,
+    similar: wordData.filter((word: string) => typeof word === 'string'),
+    rawData: [...wordData],
+  };
+  // redirect(`/dictionary/${word.toString().toLowerCase()}`);
 }

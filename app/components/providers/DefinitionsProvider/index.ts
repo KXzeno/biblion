4eaf7a476057ecbb34f9e833f4be91e45621ef77
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-04 12:35:12 -0800

    Add module export route for provider

---
+++ app/components/providers/DefinitionsProvider/index.ts
@@ -0,0 +1,9 @@
+import DefinitionsProvider, { DefinitionsContext, DefinitionsDispatchContext } from './DefinitionsProvider';
+
+export default DefinitionsProvider;
+
+export {
+  DefinitionsContext,
+  DefinitionsDispatchContext,
+};
+

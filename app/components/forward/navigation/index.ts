4343a892e0c9afef642e99f51b55782fce9c1d65
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-04 12:33:34 -0800

    Add module export route for navigation RFCs

---
+++ app/components/forward/navigation/index.ts
@@ -0,0 +1,8 @@
+import SearchBar from './SearchBar/SearchBar';
+import type { ReducerState, ReducerAction } from './SearchBar/SearchBar.types';
+
+export {
+  SearchBar, 
+  type ReducerState as SearchBarReducState, 
+  type ReducerAction as SearchBarReducAction,
+}

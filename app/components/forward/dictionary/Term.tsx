8fa345459e985808243a14e47631037eb5fb7fe5
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-05 14:42:58 -0800

    chore: remove `useSearchParams`
    - Unused, halts build

--- app/components/forward/dictionary/Term.tsx
+++ app/components/forward/dictionary/Term.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 import { 
   DefinitionsContext,

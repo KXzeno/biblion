4375da643c663bf1524ee452201f6208b9842fec
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-05 14:38:07 -0800

    fix: reroute only on mount and inject payload

--- app/components/forward/dictionary/Term.tsx
+++ app/components/forward/dictionary/Term.tsx
@@ -1,20 +1,33 @@
 'use client';
 
 import React from 'react';
-
-import { DefinitionsContext } from '@/providers/DefinitionsProvider';
 import { useRouter, useSearchParams } from 'next/navigation';
 
+import { 
+  DefinitionsContext,
+  DefinitionsDispatchContext
+} from '@/providers/DefinitionsProvider';
+import { ActionType } from '@/forward/navigation/SearchBar/SearchBar.types';
+
+
 export default function Term({ term }: { term: string }): React.ReactNode {
-  const useDefinitionsContext = () => React.useContext(DefinitionsContext);
   const router = useRouter();
 
-  const getSearchParams = new URLSearchParams(window.location.search);
-  const encodedDefs = getSearchParams.get('defs') as string;
-  const defs = JSON.parse(decodeURI(encodedDefs));
+  const useDefinitionsContext = () => React.useContext(DefinitionsContext);
+  const { reducState } = useDefinitionsContext();
 
-  // router.replace(window.location.href.replaceAll(/(?<=\?)[\?\S\s]+/g, ''));
-  // console.log(defs);
+  const useDefinitionsDispatchContext = () => React.useContext(DefinitionsDispatchContext);
+  const { dispatch } = useDefinitionsDispatchContext();
+
+  React.useEffect(() => {
+    const getSearchParams = new URLSearchParams(window.location.search);
+    const encodedDefs = getSearchParams.get('defs') as string;
+    const defs = JSON.parse(decodeURI(encodedDefs));
+    router.replace(window.location.href.replaceAll(/(?<=\?)[\?\S\s]+/g, ''));
+    dispatch({ type: ActionType.Inject, payload: { ...reducState, rawData: defs } });
+    console.log(defs);
+  }, []);
+
 
   return (
     <>

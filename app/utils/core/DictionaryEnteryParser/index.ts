0a27722358563ed3a7b7a44365fcfceca5b5489b
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-05 16:21:39 -0800

    chore: include type `ParsedPayload` in exports

--- app/utils/core/DictionaryEnteryParser/index.ts
+++ app/utils/core/DictionaryEnteryParser/index.ts
@@ -1,9 +1,10 @@
 import DictionaryEntryParser from './DictionaryEntryParser';
 
-import type { Payload } from './types/DictionaryEntryParser.types';
+import type { Payload, ParsedPayload } from './types/DictionaryEntryParser.types';
 
 export default DictionaryEntryParser;
 
 export {
   Payload,
+  ParsedPayload,
 };

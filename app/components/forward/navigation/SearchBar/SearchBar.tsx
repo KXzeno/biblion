525fa436f3ea6e9e42526d25b1970f4ae22b559c
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-05 17:24:54 -0800

    fix: fortify checks
    + Prematurely terminate effect if rawData is empty
    + Check for `similar` length instead of null

--- app/components/forward/navigation/SearchBar/SearchBar.tsx
+++ app/components/forward/navigation/SearchBar/SearchBar.tsx
@@ -13,12 +13,14 @@ export default function SearchBar() {
   const router = useRouter();
 
   React.useEffect(() => {
-    if (formState.rawData.length > 0 && typeof formState.rawData[0] !== 'string') {
+    if (formState.rawData.length === 0) {
+      return;
+    }
+    if (typeof formState.rawData[0] !== 'string') {
       /** State is not restored due to disparate component trees */
       dispatch({ type: ActionType.Inject, payload: { ...reducState, rawData: formState.rawData } });
       const word = Object.values(formState.rawData[0])[0].toLowerCase();
       formState.rawData.shift();
-      console.log(formState.rawData);
       /** Session storage approach */
       window.sessionStorage.setItem("injected", JSON.stringify(formState.rawData));
       router.push(`/dictionary/${word}`);
@@ -30,59 +32,59 @@ export default function SearchBar() {
        * const queryString = `${encodeURIComponent(JSON.stringify(formState.rawData))}`
        * router.push(`/dictionary/${word}?defs=${queryString}`);
        */
-      }
-      }, [formState.rawData]);
-
-      return (
-        <div className='flex flex-col w-screen h-screen border-gray-800 border-8 text-center'>
-          <div className='inline-flex justify-center items-center bg-slate-900 h-7 w-64 self-center translate-y-34 rounded-lg text-violet-300 border-1 border-violet-900'>
-            <form 
-              action={formAction} 
-              onSubmit={(e) => {
-                dispatch({ type: ActionType.Query, payload: { ...reducState, input: reducState.input } });
-                dispatch({ type: ActionType.Invalidate });
-                e.stopPropagation();
-              }}
-            >
-              <input
-                placeholder='Search a word'
-                name='word'
-                value={reducState.input}
-                onChange={(e) => {
-                  dispatch({
-                    type: ActionType.Input,
-                    payload: { ...reducState, input: e.target.value },
-                  });
-                }}
-                className='pl-2 h-5 outline-none'
-                autoComplete='off'
-              />
-            </form>
-            {formState.msg && 
-            <>
-              <p className='absolute -translate-y-12'>
-                {`Search for \'${formState.msg}\' failed.`}
-              </p>
-            </>
-            }
-          </div>
-          <>
-            {formState.msg && formState.similar && 
-            <div className='flex self-center justify-center relative translate-y-34 w-screen h-64'>
-              <div className='absolute flex flex-col w-96 h-max translate-y-20'>
-                <span>Did you mean any of:</span> 
-                <div className='grid grid-cols-3 mt-8'>{formState.similar.map((pot: string) => {
-                  return (
-                    <p key={`${pot}`}>
-                      {pot}
-                    </p>
-                  );
-                })}
-                </div>
-              </div>
-            </div>
-            }
-          </>
-        </div>
-      );
     }
+  }, [formState.rawData]);
+
+  return (
+    <div className='flex flex-col w-screen h-screen border-gray-800 border-8 text-center'>
+      <div className='inline-flex justify-center items-center bg-slate-900 h-7 w-64 self-center translate-y-34 rounded-lg text-violet-300 border-1 border-violet-900'>
+        <form 
+          action={formAction} 
+          onSubmit={(e) => {
+            dispatch({ type: ActionType.Query, payload: { ...reducState, input: reducState.input } });
+            dispatch({ type: ActionType.Invalidate });
+            e.stopPropagation();
+          }}
+        >
+          <input
+            placeholder='Search a word'
+            name='word'
+            value={reducState.input}
+            onChange={(e) => {
+              dispatch({
+                type: ActionType.Input,
+                payload: { ...reducState, input: e.target.value },
+              });
+            }}
+            className='pl-2 h-5 outline-none'
+            autoComplete='off'
+          />
+        </form>
+        {formState.msg && 
+        <>
+          <p className='absolute -translate-y-12'>
+            {`Search for \'${formState.msg}\' failed.`}
+          </p>
+        </>
+        }
+      </div>
+      <>
+        {formState.msg && formState.similar.length > 0 && 
+        <div className='flex self-center justify-center relative translate-y-34 w-screen h-64'>
+          <div className='absolute flex flex-col w-96 h-max translate-y-20'>
+            <span>Did you mean any of:</span> 
+            <div className='grid grid-cols-3 mt-8'>{formState.similar.map((pot: string) => {
+              return (
+                <p key={`${pot}`}>
+                  {pot}
+                </p>
+              );
+            })}
+            </div>
+          </div>
+        </div>
+        }
+      </>
+    </div>
+  );
+}

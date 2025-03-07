95477ea653743a07a077286343539ac559d7235e
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-07 05:34:45 -0800

    Extract inline types from `query` action

---
+++ app/components/actions/types/query.types.ts
@@ -0,0 +1,7 @@
+export type SuccessfulResponse = Array<{ target: string, error: string } | object | string>;
+
+export interface ErroneousResponse {
+  msg: string;
+  similar: Array<string>;
+  rawData: Array<{ target: string; error: string } | object>;
+}

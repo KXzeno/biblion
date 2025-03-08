c2cf1f6754d8a759dcfdf9c7f749aa17035560be
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-08 06:31:54 -0800

    Access cookie for manual HTTP-based validation

--- app/dictionary/[slug]/page.tsx
+++ app/dictionary/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { cookies as getCookies } from 'next/headers';
 
 import Dictionary from '@/components/main/Dictionary';
 
@@ -8,6 +9,15 @@ export default async function WordPage({
   params: Promise<{ slug: string }>
 }) {
   const { slug } = await params;
+  const cookies = await getCookies();
+
+  const query = cookies.has('query') ? cookies.get('query')!.value : null;
+
+  if (query !== null) {
+    if (query === slug) {
+      // TODO: Validate 
+    }
+  }
 
   return (
     <Dictionary word={slug} />

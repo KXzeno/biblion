3ffb068ab817c1fc80c97d8cf6fb21d9e8b3d45c
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-18 09:09:24 -0700

    refactor: extract utility fns

--- app/api/v1/route.ts
+++ app/api/v1/route.ts
@@ -1,3 +1,5 @@
+import getWordData, { makeWellFormed } from "@/utils/auxil/getWordData";
+
 /**
  * @remarks
  *
@@ -9,63 +11,32 @@ enum RateLimitActivation {
 */
 
 /**
- * Well-forms string inputs for blob constructions
- *
- * @param parts - an array of strings to well-form
- * @returns the parts well-formed
- */
-function makeWellFormed(parts: Array<string>): Array<string> {
-  const newParts = parts.map(part => part.toWellFormed());
-  return newParts;
-}
-
-/**
- * Uses Merriam-Webster's dictionary API 
- * to fetch word data from a given input
- *
- * @param word - the word for endpoint
- * @returns an object conforming to Merriam's
- * data models, else null if response is 400-500
- */
-async function getWordData(word: string): Promise<unknown[] | null> {
-  // Reference env vars
-  const apiKey = process.env.API_KEY;
-  const dictionaryEndpoint = process.env.ENDPOINT;
-
-  // Use string interpolation to construct full API URL
-  const req = `${dictionaryEndpoint}${word}?key=${apiKey}`;
-
-  // Retrieve response
-  const res = await fetch(req).then(res => res.json());
-
-  return res
-}
-
-/**
  * @remarks
  *
+ * Use cookie to avoid queries when reaching limits
+ *
  * To be used
-* export async function GET(req: Request) {
-*   // TODO: Cache ip addresses for rate limiting
-*   const isRateLimited = false;
-* 
-*   // const headers = req.headers.entries();
-*   // const client = headers.find(header => header[0] === 'x-forwarded-for');
-*   // let clientIp;
-*   // if (client) {
-*   //   clientIp = client[1];
-*   // }
-* 
-*   // console.log(clientIp);
-* 
-*   const msg = isRateLimited ? RateLimitActivation.ON : RateLimitActivation.OFF;
-* 
-*   const parts = makeWellFormed([msg]);
-*   const blob = new Blob(parts, { type: 'text/plain' });
-* 
-*   return new Response(blob);
-* }
 */
+// export async function GET(req: Request) {
+  // TODO: Cache ip addresses for rate limiting
+  // const isRateLimited = false;
+
+  // const headers = req.headers.entries();
+  // const client = headers.find(header => header[0] === 'x-forwarded-for');
+  // let clientIp;
+  // if (client) {
+  //   clientIp = client[1];
+  // }
+
+  // console.log(clientIp);
+
+  // const msg = isRateLimited ? RateLimitActivation.ON : RateLimitActivation.OFF;
+
+  // const parts = makeWellFormed([msg]);
+  // const blob = new Blob(parts, { type: 'text/plain' });
+
+  // return new Response(blob);
+// }
 
 /**
  * The route's post request which fetches 

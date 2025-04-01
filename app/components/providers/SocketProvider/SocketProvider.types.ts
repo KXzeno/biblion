e76d0a264e9373daddb641f3d0d387b464e98525
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-04-01 12:30:31 -0700

    Append props to context interface
    + `pendingSignal`
    + `sendFromProxy()`
    + `client`

--- app/components/providers/SocketProvider/SocketProvider.types.ts
+++ app/components/providers/SocketProvider/SocketProvider.types.ts
@@ -1,6 +1,11 @@
+import ColligateWebSocket from "@/utils/core/WebSockets";
+
 export interface ContextData {
   send: () => void;
   signals: Array<string>;
+  pendingSignal: string;
   rates: string;
   setRates: React.Dispatch<React.SetStateAction<string>> | (() => void);
+  sendFromProxy: () => void;
+  client: ColligateWebSocket | null;
 }

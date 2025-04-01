6637395a8fd21c057e9c9b74b371fa8290fbdbd2
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-04-01 12:31:15 -0700

    feat: implement memoized `sendFromProxy` method
    + Accesses web worker to notify main thread for socket communication

--- app/components/providers/SocketProvider/SocketProvider.tsx
+++ app/components/providers/SocketProvider/SocketProvider.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import "dotenv/config";
 
+import ColligateWebSocket from "@/utils/core/WebSockets";
 import { SharedWorkerCarrier } from "@/utils/workers";
 import { handleWorkerEvent } from "@/utils/auxil/socketWorkerTasks";
 import { SocketWorkerEvent } from "@/utils/workers/types/workers.types";
@@ -11,14 +12,30 @@ import { type ContextData } from "@/providers/SocketProvider/SocketProvider.type
 export const SocketContext: React.Context<ContextData> = React.createContext({
   send: () => {},
   signals: [""],
+  pendingSignal: "",
   rates: "",
   setRates: (() => {}) as ContextData['setRates'],
+  sendFromProxy: () => {},
+  client: null as ColligateWebSocket | null,
 });
 
 export default function WebSocket({ children }: { children: React.ReactNode }) {
   const [signals, setSignals] = React.useState<Array<string>>([]);
+  const [pendingSignal, setPendingSignal] = React.useState<string>("");
   const [rates, setRates] = React.useState<string>("");
   const [carrier, setCarrier] = React.useState<SharedWorkerCarrier>({ worker: null, id: null });
+  const [client, setClient] = React.useState<ColligateWebSocket | null>(null);
+
+  // TODO: Create logic that updates new rates for all clients
+  // TODO: Transform states to reducer and create dispatcher context
+  const sendFromProxy = React.useCallback(() => {
+    console.log(pendingSignal);
+    if (client && pendingSignal.length > 0) {
+      console.log('Client sends?');
+      client.send({ sighted: `${pendingSignal}` });
+    }
+    setPendingSignal("");
+  }, [pendingSignal]);
 
   React.useEffect(() => {
     if (!!window.SharedWorker) {
@@ -39,13 +56,52 @@ export default function WebSocket({ children }: { children: React.ReactNode }) {
           case 'boolean': {
             if (e.data === true) {
               // Initialize socket
+
+              /** For production */
+              // const { C_ENDPOINT, C_DESTINATION, C_BROADCAST } = process.env;
+
+              /** For development */
+              const [ C_ENDPOINT, C_DESTINATION, C_BROADCAST ] = ["wss://biblion-colligate.onrender.com/colligate-websocket", "/app/observe/0", "/temp/audits/0" ];
+
+              const stompClient = new ColligateWebSocket({
+                endpoint: C_ENDPOINT,
+                destination: C_DESTINATION,
+                broadcast: C_BROADCAST,
+              });
+
+              stompClient.handleConnect({
+                extFn() {
+                },
+                intFn(content) {
+                  setSignals((prev) => [...prev, content]);
+                  try {
+                    fetch("https://discord.com/api/webhooks/1355217343109795933/qe744baN1kkgcCK5-GmjlNAoctTUlhPNBhvageQtjykPX8aA9kLSKx4p_7wRKOPUe7gv", {
+                      method: 'POST',
+                      headers: {
+                        'Content-Type': 'application/json',
+                      },
+                      body: JSON.stringify({
+                        content: `${content}`,
+                      }),
+                    });
+                  } catch (e) {
+                    console.error(e);
+                  }
+                },
+              });
+
+              stompClient.handleErrors();
+
+              stompClient.activate();
+
+              setClient(() => stompClient);
               break;
             }
-            // Update signals
+            // False
             break;
           }
           case 'string': {
-            setSignals([...signals, e.data]);
+            setPendingSignal(e.data);
             break;
           }
         }
@@ -67,7 +123,11 @@ export default function WebSocket({ children }: { children: React.ReactNode }) {
   function sendMsg() {
     if (carrier.worker) {
       const { port } = carrier.worker;
-      port.postMessage(rates);
+      if (client) {
+        client.send({ sighted: rates });
+        return;
+      }
+      port.postMessage(`SEND:${rates}`);
       setRates("");
     }
   }
@@ -77,7 +137,10 @@ export default function WebSocket({ children }: { children: React.ReactNode }) {
       value={{ 
         send: sendMsg,
         signals,
-        rates, setRates
+        rates, setRates,
+        sendFromProxy,
+        client,
+        pendingSignal,
       }}
     >
       {children}

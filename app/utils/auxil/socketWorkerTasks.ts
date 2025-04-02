4fd6da69f43d1f9ecf800f56dfe1ed23e2117bea
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-04-01 17:20:56 -0700

    FEAT: implement reducer for `SocketProvider`

--- app/utils/auxil/socketWorkerTasks.ts
+++ app/utils/auxil/socketWorkerTasks.ts
@@ -1,6 +1,17 @@
 import "dotenv/config";
 
 import { SharedWorkerCarrier, SocketWorkerEvent } from "@/utils/workers/types/workers.types";
+import { 
+  type ReducerState,
+  type ReducerAction,
+  type Payload,
+  type RatesPayload,
+  type SignalPayload,
+  type CarrierPayload,
+  type ClientPayload,
+  type PendingSignalPayload,
+  ActionType,
+} from "@/components/providers/SocketProvider/SocketProvider.types";
 
 export function remountListener(content: Array<string>) {
   try {
@@ -24,4 +35,61 @@ export function handleWorkerEvent(carrier: SharedWorkerCarrier, type: SocketWork
   }
 
   carrier.worker.port.postMessage(`${type}:${carrier.id}${ambiguous ? `:${ambiguous}` : ''}`);
+}
+
+export function socketProviderReducer(state: ReducerState, action: ReducerAction): ReducerState {
+  switch (action.type) {
+    case ActionType.Rate: {
+      if (!(action.payload && isRatesPayload(action.payload))) {
+        throw new Error('No rates defined in payload');
+      }
+      return { ...state, rates: action.payload.rates };
+    }
+    case ActionType.Carrier: {
+      if (!(action.payload && isCarrierPayload(action.payload))) {
+        throw new Error('No carrier defined in payload');
+      }
+      return { ...state, carrier: action.payload.carrier };
+    }
+    case ActionType.Client: {
+      if (!(action.payload && isClientPayload(action.payload))) {
+        throw new Error('No client defined in payload');
+      }
+      return { ...state, client: action.payload.client };
+    }
+    case ActionType.Signal: {
+      if (!(action.payload && isSignalPayload(action.payload))) {
+        throw new Error('No signals defined in payload');
+      }
+      return { ...state, signals: [...state.signals, action.payload.signal] };
+    }
+    case ActionType.Offload: {
+      if (!(action.payload && isPendingSignalPayload(action.payload))) {
+        throw new Error('No pending signal defined in payload');
+      }
+      return { ...state, pendingSignal: action.payload.pendingSignal };
+
+    }
+    default: return state;
+  }
+}
+
+function isRatesPayload(payload: Payload): payload is RatesPayload {
+  return 'rates' in payload;
+}
+
+function isSignalPayload(payload: Payload): payload is SignalPayload {
+  return 'signal' in payload;
+}
+
+function isClientPayload(payload: Payload): payload is ClientPayload {
+  return 'client' in payload;
+}
+
+function isPendingSignalPayload(payload: Payload): payload is PendingSignalPayload {
+  return 'pendingSignal' in payload;
+}
+
+function isCarrierPayload(payload: Payload): payload is CarrierPayload {
+  return 'carrier' in payload;
 }

27499ebb7823e7ad7d22e862b45c76411d756689
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-04-02 18:46:32 -0700

    Extract and drill env props

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,23 @@
+'use server';
+
 import React from "react";
 
 import WebSocket from "@/main/WebSocket";
 
 import SocketProvider from "@/components/providers/SocketProvider";
 
-export default function Page() {
+export default async function Page() {
+  const { C_ENDPOINT, C_DESTINATION , C_BROADCAST, DISCORD_WH_ENDPOINT } = process.env;
+
   return (
-    <SocketProvider>
+    <SocketProvider 
+      ColligateWebSocketConstructorParams={{
+        C_ENDPOINT,
+        C_DESTINATION,
+        C_BROADCAST,
+      }}
+      DISCORD_WH_ENDPOINT={DISCORD_WH_ENDPOINT}
+    >
       <WebSocket />
     </SocketProvider>
   );

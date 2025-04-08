6e0ad5bc5f65c30348f7242f6a2c74bb7dcaed70
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-04-07 18:05:07 -0700

    Allow `postimg` domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import { RemotePattern } from "next/dist/shared/lib/image-config";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      // new URL('https://i.postimg.cc/') as RemotePattern,
+      {
+        protocol: "https",
+        hostname: "i.postimg.cc",
+        pathname: "/**"
+      }
+    ],
+  }
 };
 
 export default nextConfig;

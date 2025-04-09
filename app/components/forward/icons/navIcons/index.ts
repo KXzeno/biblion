583c3a48739846f13a30fefc3dc4e85b5bb27fa3
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-04-09 12:30:55 -0700

    Update `navIcons` export route

--- app/components/forward/icons/navIcons/index.ts
+++ app/components/forward/icons/navIcons/index.ts
@@ -1,7 +1,9 @@
 import ProfileIcon from "./ProfleIcon";
 import LogoIcon from "./LogoIcon";
+import WrenchAndDriverIcon from "./WrenchAndDriverIcon";
 
 export {
   ProfileIcon,
   LogoIcon,
+  WrenchAndDriverIcon,
 }

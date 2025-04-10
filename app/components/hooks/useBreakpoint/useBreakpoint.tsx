79233835ba31f4ce939081420033169483696a56
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-04-09 18:26:39 -0700

    feat: implement breakpoint hook

---
+++ app/components/hooks/useBreakpoint/useBreakpoint.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { BreakpointHookProps } from './useBreakpoint.types';
+
+export default function useBreakpoint({ 
+  breakPoint, 
+}: BreakpointHookProps = { 
+  breakPoint: 768,
+}): [boolean, number] {
+  const [x, setX] = React.useState<number>(0);
+  const [crossed, setCrossed] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    const handleResizeUpdates = () => {
+      const width = window.innerWidth;
+      setX(() => width);
+      if (width >= breakPoint) {
+        setCrossed(() => true);
+      } else if (crossed) {
+        setCrossed(() => false)
+      }
+    };
+
+    window.addEventListener("resize", handleResizeUpdates);
+
+    return () => {
+      window.removeEventListener("resize", handleResizeUpdates);
+    }
+  }, []);
+
+  return [crossed, x];
+}

52db41402e45d35469838283e8f0f34f9245587e
git <82054121+KXzeno@users.noreply.github.com>
2025-04-17 00:50:32 -0700

    feat: conditionally apply menu/profile icons

--- app/components/forward/navigation/NavBar/NavBar.tsx
+++ app/components/forward/navigation/NavBar/NavBar.tsx
@@ -3,10 +3,13 @@ import Link from 'next/link';
 
 import { ProfileIcon, LogoIcon } from '@/forward/icons/navIcons';
 import { Version } from '@/forward/marks';
+import useBreakpoint from '@/components/hooks/useBreakpoint';
+import MenuButton from './MenuButton';
 import "./NavBar.css";
 
 export default function NavBar() {
-  // TODO: make list take up 2 cells for now
+  const [crossed] = useBreakpoint({ breakPoint: 628 });
+
   return (
     <nav className='navbar'>
       <aside className='ver-tag'>
@@ -21,9 +24,11 @@ export default function NavBar() {
           <div dir="ltr" className='menu-nav-item'><Link href="#example">Example</Link></div>
           <div dir="rtl" className='menu-nav-item'><Link href="/me/lists">Lists</Link></div>
         </li>
-        <li className='profile-nav-item'>
-          <ProfileIcon svgClassName='scale-[0.8]' />
-        </li>
+        {
+          !crossed 
+            ? <li className='profile-nav-item'><ProfileIcon svgClassName='scale-[0.8]' /></li>
+            : <li className='menu-btn'><MenuButton /></li>
+        }
       </ul>
     </nav>
   )
